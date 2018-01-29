3abbf7d8d0c70d3db620023e747b4b31803eaef8
Ofer Ben-Yacov <ofer.benyacov@gmail.com>
2018-01-29 10:31:33 +0200

    cnn with estimator

---
+++ cnn/cnn2_predict.py
@@ -0,0 +1,103 @@
+#  Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+import dataset.cfar10 as ds
+import matplotlib.pyplot as plt
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+def features_to__image(features):
+    return features.reshape(3, 32, 32).transpose(1, 2, 0).astype('uint8')
+
+
+def cnn_model_fn(features, labels, mode):
+    input_layer = tf.reshape(features["x"], [-1, 32, 32, 3])
+
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+    pool2_flat = tf.reshape(pool2, [-1, 8 * 8 * 64])
+
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    logits = tf.layers.dense(inputs=dropout, units=10)
+
+    predictions = {
+        "classes": tf.argmax(input=logits, axis=1),
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
+dataset = ds.get_dataset()
+
+train_data = np.array(dataset['data'], dtype=np.float32)
+train_labels = np.array(dataset['labels'], dtype=np.int32)
+
+cnn_classifier = tf.estimator.Estimator(
+    model_fn=cnn_model_fn, model_dir="/tmp/cnn_convnet_model")
+
+indx = 11
+cnn_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": train_data[indx].reshape((1, -1))}, shuffle=False)
+
+result = cnn_classifier.predict(cnn_input_fn)
+
+print(result.next())
+
+test0 = features_to__image(train_data[indx])
+plt.figure(num=None, figsize=(1, 1))
+plt.imshow(test0)
+plt.show()
